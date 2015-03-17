49ddb1161297d018bbcb1a3b84ebf268055c0b43
Chris Seelus <chris@imeos.com>
2015-03-17 03:03:02 +0100

    Update README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Fish Theme: zen
+
+Spacious [fish shell](http://fishshell.com/) and superclean fish shell theme for people who use git.
+
+![zen theme](https://raw.github.com/cseelus/zen-fish/master/zen-fish_preview.png)
---
+++ fish_prompt.fish
@@ -0,0 +1,88 @@
+# name: clearance
+# ---------------
+# Based on idan. Display the following bits on the left:
+# - Virtualenv name (if applicable, see https://github.com/adambrenecki/virtualfish)
+# - Current directory name
+# - Git branch and dirty state (if inside a git repo)
+
+function _git_branch_name
+  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
+
+function _git_is_dirty
+  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+end
+
+function _remote_hostname
+  if test -n "$SSH_CONNECTION"
+    echo (whoami)@(hostname)
+  end
+end
+
+function fish_prompt
+  set -l turquoise (set_color green)
+  set -l cobalt (set_color blue)
+  set -l purple (set_color cyan)
+  set -l wine (set_color red)
+  set -l cloud (set_color white)
+  set -l sap (set_color yellow)
+  # When some day terminals get real colors …
+  # set -l darkgrey (set_color 535557)
+  # set -l turquoise (set_color 9bfda6)
+  # set -l cobalt (set_color 406ddf)
+  # set -l purple (set_color 7a99ff)
+  # set -l wine (set_color a84d70)
+  # set -l cloud (set_color e6e7e7)
+  # set -l sap (set_color fde9a2)
+
+  set -l cwd $cobalt(pwd | sed "s:^$HOME:~:")
+
+  # Output the prompt, left to right
+
+  # Add a newline before new prompts
+  echo -e ''
+
+  # Display [venvname] if in a virtualenv
+  if set -q VIRTUAL_ENV
+      echo -n -s (set_color -b turquoise black) '[' (basename "$VIRTUAL_ENV") ']' $cloud ' '
+  end
+
+  # Print remote hostname
+  if [ (_remote_hostname) ]
+    echo -n $cobalt (_remote_hostname)$cloud '· '
+  end
+
+  # Print pwd or full path
+  echo -n -s $cwd (set_color normal) $cloud
+
+  # Show git branch and status
+  if [ (_git_branch_name) ]
+    set -l git_branch (_git_branch_name)
+
+    if [ (_git_is_dirty) ]
+      set git_info '(' $sap $git_branch "±" $cloud ')'
+    else
+      set git_info '(' $turquoise $git_branch $cloud ')'
+    end
+    echo -n -s ' · ' $git_info $cloud
+  end
+
+  # Terminate with a nice prompt char
+  echo -e ''
+  echo -e -n -s '> ' $cloud
+end
+
+# set fish_color_autosuggestion black
+set fish_color_normal white
+set fish_color_command green
+set fish_color_param magenta
+# fish_color_quote, the color for quoted blocks of text
+# fish_color_redirection, the color for IO redirections
+# fish_color_end, the color for process separators like ';' and '&'
+# fish_color_error, the color used to highlight potential errors
+# fish_color_comment, the color used for code comments
+# fish_color_match, the color used to highlight matching parenthesis
+# fish_color_search_match, the color used to highlight history search matches
+# fish_color_operator, the color for parameter expansion operators like '*' and '~'
+# fish_color_escape, the color used to highlight character escapes like '\n' and '\x70'
+# fish_color_cwd, the color used for the current working directory in the default prompt
